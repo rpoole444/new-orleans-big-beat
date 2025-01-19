4010509716a048c0fbc09d3ffc2413ffb801de5d
Reid Poole <poole.reid@gmail.com>
2025-01-19 10:22:19 -0700

    add logo for band

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import bandIcon from "../../public/Colorado Springs.jpg"
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-xl font-bold">
-          <Link href="/">New Orleans Big Beat</Link>
+        <div className="shadow-lg rounded">
+          <Link href="/">
+            <Image
+              src={bandIcon}
+              alt={`Big Beat Band Icon`}
+              width={200}
+              height={200}
+            />
+          </Link>
         </div>
         <div className="space-x-4">
           <Link href="/music" className="hover:text-gray-400">Music</Link>

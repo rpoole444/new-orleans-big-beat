056649871151fd0be27f4e9291c9842b86b09c97
Reid Poole <poole.reid@gmail.com>
2025-01-21 21:32:38 -0700

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'New Orleans Big Beat',
+  description: 'The official website of New Orleans Big Beat',
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
